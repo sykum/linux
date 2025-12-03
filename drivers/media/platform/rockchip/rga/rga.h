c3477e2e02175f90fb0ed1363474425dd09fb18b
Sven Püschel <s.pueschel@pengutronix.de>
2025-12-03 16:52:30 +0100

    media: rockchip: rga: move cmdbuf to rga_ctx
    Move the command buffer to the rga_ctx struct in preparation to reuse
    an already prepared command buffer. Having a command buffer per context
    allows to reuse the command buffer for other frames even when
    multiple applications are using the RGA in at the same time.
    
    Signed-off-by: Sven Püschel <s.pueschel@pengutronix.de>
    Reviewed-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>

--- drivers/media/platform/rockchip/rga/rga.h
+++ drivers/media/platform/rockchip/rga/rga.h
@@ -56,6 +56,9 @@ struct rga_ctx {
 	struct rga_frame out;
 	struct v4l2_ctrl_handler ctrl_handler;
 
+	void *cmdbuf_virt;
+	dma_addr_t cmdbuf_phy;
+
 	int osequence;
 	int csequence;
 
@@ -91,8 +94,6 @@ struct rockchip_rga {
 	spinlock_t ctrl_lock;
 
 	struct rga_ctx *curr;
-	dma_addr_t cmdbuf_phy;
-	void *cmdbuf_virt;
 
 	const struct rga_hw *hw;
 };
